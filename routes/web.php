63aa9f5f10a0d1384f6996d60f949c8a01e45cf2
Edwin Velasquez <105985102+clinicarehn@users.noreply.github.com>
2024-09-23 19:32:44 -0600

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+<<<<<<< HEAD
+=======
+    // return view('welcome');
+>>>>>>> 5858007d7b82745995ddcf47d8514c1406f25904
+    return redirect('/admin/login');
+});
